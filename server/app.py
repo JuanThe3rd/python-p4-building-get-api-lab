6395c0149bd46a0b2afe282946199e7adaf8bff4
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-07-27 14:00:53 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,86 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+
+    for bakery in Bakery.query.all():
+        bakery_dict = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at,
+            'updated_at': bakery.updated_at
+        }
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries), 
+        200
+        )
+    
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = {
+        'id': bakery.id,
+        'name': bakery.name,
+        'created_at': bakery.created_at
+    }
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200,
+    )
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price).all():
+        baked_good_dict = {
+            'id': baked_good.id,
+            'name': baked_good.name,
+            'price': baked_good.price,
+            'created_at': baked_good.created_at,
+            'updated_at': baked_good.updated_at
+        }
+        baked_goods.append(baked_good_dict)
+
+    response = make_response(
+        jsonify(baked_goods),
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.all()
+    max = 0
+    baked_good = None
+
+    for good in baked_goods:
+        if good.price > max:
+            max = good.price
+            baked_good = good
+
+    exp_baked_good_dict = {
+        'id': baked_good.id,
+        'name': baked_good.name,
+        'price': baked_good.price,
+        'created_at': baked_good.created_at,
+        'updated_at': baked_good.updated_at
+    }
+
+    response = make_response(
+        jsonify(exp_baked_good_dict),
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
